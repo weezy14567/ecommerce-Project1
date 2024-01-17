6a07324faa3dc06d20c9ff31864d940c370487af
weezy14567 <154681716+weezy14567@users.noreply.github.com>
2024-01-17 10:27:11 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,11 +46,9 @@ const connect = () => {
 };
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+
 app.use('/api/seed', seedRouter);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
-});
+
 
 app.listen(process.env.PORT, () => {
   console.log('server connected');
